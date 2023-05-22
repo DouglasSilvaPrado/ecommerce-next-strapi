b17b7753707588ff0f09424a4a5b2514bafc6521
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2023-05-22 11:44:36 -0300

    feat: add navbar and dropdown component

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -7,23 +7,37 @@ import { BiMenu } from 'react-icons/bi'
 import { FaUser } from 'react-icons/fa'
 import { BsSearch } from 'react-icons/bs'
 import { Rubik } from 'next/font/google'
+import Dropdown from '../Dropdown/dropdown'
 
 const rubik = Rubik({ subsets: ['latin'] })
 
 
 export const Navbar = () => {
+  const items = { label: 'Men', values: 
+    [
+      {label: 'men 1', path: '/men1'}, 
+      {label: 'men 2', path: '/men2'}
+    ]
+  }
+  const items2 = { label: 'Women', values: 
+    [
+      {label: 'women 1', path: '/women1'}, 
+      {label: 'women 2', path: '/women2'}
+    ]
+  }
+
 
   const [toggleMenu, setToggleMenu] = useState(false)
 
   return (
-    <nav className={`bg-faWhite h-14 ${toggleMenu ? 'rounded-t-xl' : 'rounded-xl'} md:rounded-xl m-auto w-11/12 mt-8 mb-6 md:h-24`}>
+    <nav className={`bg-faWhite  h-14 ${toggleMenu ? 'rounded-t-xl' : 'rounded-xl'} md:rounded-xl m-auto w-11/12 mt-8 mb-6 md:h-24`}>
       <div className='flex items-center h-full justify-between mx-4'>
 
         <div className='hidden md:inline-block md:w-[18.75rem]'>
           <ul className={`flex text-darkGray font-semibold text-base gap-2 ${rubik.className}`}>
             <li>New Drops🔥</li>
-            <li>Men</li>
-            <li>Women</li>
+            <li><Dropdown items={items} /> </li>
+            <li><Dropdown items={items2} /></li>
           </ul>
         </div>
 
@@ -47,11 +61,11 @@ export const Navbar = () => {
         </div>
       </div>
       {toggleMenu &&
-        <div className='bg-faWhite rounded-b-xl py-2 px-4 w-11/12 md:hidden'>
+        <div className='bg-faWhite absolute w-11/12 rounded-b-xl py-2 px-4 md:hidden'>
           <ul className={`text-darkGray text-sm font-semibold divide-y-2 ${rubik.className}`}>
             <li>New Drops 🔥</li>
-            <li>Men</li>
-            <li>Women</li>
+            <li><Dropdown items={items} /> </li>
+            <li><Dropdown items={items2} /></li>
           </ul>
         </div>
       }
