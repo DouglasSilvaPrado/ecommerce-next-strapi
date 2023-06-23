4a0c364a56089a4d253bf4bae50bb7668aa3b7c4
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2023-06-23 16:04:55 -0300

    feat: add props onClick

--- frontend/src/components/Buttons/Secondary.tsx
+++ frontend/src/components/Buttons/Secondary.tsx
@@ -4,11 +4,15 @@ interface SecondaryProps {
   text: string | React.ReactNode
   price?: number
   className?: string
+  onClick?: () => void
 }
 
-export const SecondaryButton = ({text, className, price}: SecondaryProps) => {
+export const SecondaryButton = ({text, className, price, onClick}: SecondaryProps) => {
   return (
-    <button className={`bg-darkGray rounded-lg text-faWhite font-medium ${className} hover:bg-blue`}>
+    <button 
+      className={`bg-darkGray rounded-lg text-faWhite font-medium ${className} hover:bg-blue`}
+      onClick={onClick ? () => onClick() : () => {}}
+    >
       { text }
       {price && (
         <span className='text-yellow'> - ${ price }</span>
