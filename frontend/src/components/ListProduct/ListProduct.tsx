f6826ab8e94b10a2e5388c43debb2831adea73ea
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2023-06-27 13:43:41 -0300

    feat: endpoint with filter in list products

--- frontend/src/components/ListProduct/ListProduct.tsx
+++ frontend/src/components/ListProduct/ListProduct.tsx
@@ -1,15 +1,33 @@
 'use client'
 
-import { useAppStore } from '@/store/store'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ProductCard } from '../Card/Product'
+import { Shoe } from '@/@types/Shoe'
+import { getShoes } from '@/services/shoes'
+import { useAppStore } from '@/store/store'
 
 export const ListProduct = () => {
-  const { fetchShoes, shoes } = useAppStore()
+  const [shoes, setShoes] = useState<Shoe[]>([])
+  
+  const { selectedFilters } = useAppStore()
+
+ const fetchShoes = async() => {
+  const queryParams = selectedFilters.map((filter) => 
+    `filters[${filter.category}s][${filter.category}][$in]=${filter.name}`)
+    .join('&')
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/api/shoes?populate=*&${queryParams}`
+  try {
+    const shoesData = await getShoes(url)
+    setShoes(shoesData)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 
   useEffect(() => {
     fetchShoes()
-  },[])
+  },[selectedFilters])
 
   
   return (
