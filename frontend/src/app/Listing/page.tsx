1b976e77ccd2d387685d984d442b11cfe2b35c22
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2023-06-27 13:44:04 -0300

    feat: add filter i n page Listing

--- frontend/src/app/Listing/page.tsx
+++ frontend/src/app/Listing/page.tsx
@@ -1,12 +1,16 @@
 import { Banner } from '@/components/Banner/Banner';
+import { Filters } from '@/components/Filters/Filters';
 import { ListProduct } from '@/components/ListProduct/ListProduct';
+
 
 export default function Page() {
   return (
     <div>
       <div>
         <Banner />
-        <div></div>{/* filtros  */}
+        <div className='my-6'>
+          <Filters />
+        </div>
         <div className='my-6'>
           <h3 className='font-semibold text-xl'>Life Style Shoes</h3>
           <p className='font-semibold text-sm'>122 items</p>
