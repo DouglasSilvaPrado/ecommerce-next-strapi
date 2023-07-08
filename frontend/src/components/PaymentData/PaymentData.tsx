5754e3eebf7b2a5bd6378b7c08df6c4faaeaa589
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2023-07-08 15:49:45 -0300

    feat: use hook

--- frontend/src/components/PaymentData/PaymentData.tsx
+++ frontend/src/components/PaymentData/PaymentData.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
+'use client'
+
 import { Rubik } from 'next/font/google'
 import { PrimaryButton } from '../Buttons/Primary'
-import Link from 'next/link'
+import { usePaymentData } from './hooks/usePaymentData'
 const rubik = Rubik({ subsets: ['latin'] })
 
 
 export const PaymentData = () => {
+
+  const { linkPayment } = usePaymentData()
+
   return (
     <div className='bg-faWhite rounded-2xl p-4 my-6'>
       <h2 className={`${rubik.className} mb-2 font-semibold text-xl`}>Payment Data</h2>
       <p className='font-semibold text-sm my-2'>
         Payment Options: We accept Visa, Mastercard, Bank Transfer and many other payment methods.
       </p>
-      <Link href={'/#'}>
+      <a href={linkPayment} target='_blank'>
        <PrimaryButton text='Pay now!' className='py-3 w-full text-bold text-base'/>
-      </Link>
+      </a>
       <p className='font-semibold text-sm my-2'>
         You will be redirected to 
         <span className='font-bold mx-1'>
