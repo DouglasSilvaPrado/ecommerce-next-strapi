7a7277b65f849ecfa2866a217684d7f6fddcfad4
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2023-07-11 14:11:09 -0300

    fix: fix navbar logo and toggle

--- frontend/src/components/Navbar/navbar.tsx
+++ frontend/src/components/Navbar/navbar.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from 'next/image';
 import React, { useState } from 'react';
 import { BiMenu } from 'react-icons/bi';
 import { FaUser } from 'react-icons/fa';
@@ -9,6 +8,7 @@ import { Rubik } from 'next/font/google';
 import Dropdown from '../Dropdown/dropdown';
 import { useAppStore } from '@/store/store';
 import Link from 'next/link';
+import { Logo } from '../Logo/Logo';
 
 const rubik = Rubik({ subsets: ['latin'] });
 
@@ -41,7 +41,7 @@ export const Navbar = () => {
       <div className='h-full'>
         <div className='flex items-center h-full justify-between mx-4'>
           <div className='hidden md:inline-block md:w-1/3'>
-            <ul className={`flex text-darkGray font-semibold md:text-xs lg:text-base gap-2 ${rubik.className}`}>
+            <ul className={`flex gap-2 text-darkGray font-semibold md:text-sm lg:text-base ${rubik.className}`}>
               <li className='cursor-pointer'><Link href="/Listing">New 🔥</Link></li>
               <li><Dropdown items={items} /></li>
               <li><Dropdown items={items2} /></li>
@@ -53,19 +53,19 @@ export const Navbar = () => {
           </button>
 
           <Link href="/">
-            <Image src="/Logo.svg" alt="logo" width={80} height={20} className="md:w-[100px] lg:w-[138px] md:h-[32px]" />
+            <Logo className="w-20 h-5 md:w-28 md:h-7 lg:w-32 lg:h-8" color="#232321" />
           </Link>
 
           <div className='flex justify-end md:w-1/3'>
             <button className="mr-2">
-              <BsSearch className='w-4 h-4 md:w-6 md:h-6' />
+              <BsSearch className='w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6' />
             </button>
             <button className="mx-2">
-              <FaUser className='w-4 h-4 md:w-6 md:h-6' />
+              <FaUser className='w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6' />
             </button>
 
           <Link href="/Cart">
-            <div className='w-5 h-5 md:w-7 md:h-7 bg-yellow rounded-full flex items-center justify-center'>
+            <div className='w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7 bg-yellow rounded-full flex items-center justify-center'>
               <span className='font-semibold text-darkGray text-sm'> {totalItems} </span>
             </div>
           </Link>
@@ -73,8 +73,11 @@ export const Navbar = () => {
         </div>
 
         {toggleMenu && (
-          <div className='bg-faWhite absolute w-full rounded-b-xl py-2 px-4 left-0 md:hidden'>
-            <ul className={`text-darkGray text-sm font-semibold divide-y-2 ${rubik.className}`}>
+          <div 
+            className='absolute w-full rounded-b-xl py-2 px-4 left-0 md:hidden'
+            style={{backgroundColor: 'rgba(250, 250, 250, 0.8)'}}
+          >
+            <ul className={`text-darkGray text-sm font-semibold divide-y-2 ${rubik.className}`} onClick={() => {setToggleMenu(false)}} >
               <li className='cursor-pointer'><Link href="/Listing">New Drops 🔥</Link></li>
               <li><Dropdown items={items} /></li>
               <li><Dropdown items={items2} /></li>
